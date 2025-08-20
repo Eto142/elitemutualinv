f2ee9a7e54915cbc215d5213defa5dd576a41504
eto <eto.saviour80@gmail.com>
2025-08-20 23:20:25 +0100

    updateall

--- app/Http/Controllers/Admin/ManagePaymentController.php
+++ app/Http/Controllers/Admin/ManagePaymentController.php
@@ -3,14 +3,20 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Wallet;
 use Illuminate\Http\Request;
 
 class ManagePaymentController extends Controller
 {
     //
 
-    public function ManagePayment(){
+    public function ManagePayment()
+{
+    // Fetch all wallets (btc, eth, usdt) from DB
+    $wallets = Wallet::all();
 
-        return view('admin.manage_payment');
-    }
+    // Pass them to the view
+    return view('admin.manage_payment', compact('wallets'));
+}
+
 }
